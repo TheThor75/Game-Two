9bc0a21a44106cbb8e188a2ab591e238a9d3e8ed
TheThor75 <123456+username@users.noreply.github.com>
2025-01-18 18:26:28 +0000

    mise a jour du css et js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 let humberger = document.getElementById("hamburger");
 let navbar = document.getElementById("navbar");
 let titre = document.getElementById("titre1");
+let lastScrollY = window.scrollY;
+const headers = document.querySelector(".header");
 
 let rappeltitre = titre.textContent;
 titre.textContent = "";
@@ -11,12 +13,21 @@ humberger.addEventListener("click", function (e) {
   navbar.classList.toggle("active");
 });
 
-document.body.addEventListener("click", function () {
-  humberger.classList.remove("active");
-  navbar.classList.remove("active");
-});
-
 window.onload = function () {
+  window.document.addEventListener("scroll", function () {
+    if (window.scrollY > lastScrollY) {
+      headers.classList.add("hidden");
+    } else {
+      headers.classList.remove("hidden");
+    }
+    lastScrollY = window.scrollY;
+  });
+
+  document.body.addEventListener("click", function () {
+    humberger.classList.remove("active");
+    navbar.classList.remove("active");
+  });
+
   for (let i = 0; i < rappeltitre.length; i++) {
     setTimeout(function () {
       titre.textContent = rappeltitre.substring(0, i);
